2c8761d9cc66a8a7ab0d98615874380411d9ae10
Yael Pereiro <Yaelppe@gmail.com>
2022-06-19 21:09:02 -0300

    pulling some movement for monk

--- Script/Monk.gd
+++ Script/Monk.gd
@@ -7,10 +7,11 @@ onready var current_anim: String = $player_monk.get_current_animation()
 onready var rayfloor: = $rayfloor
 onready var floor_ray1: =  $rayfloor/floor_ray1
 onready var floor_ray2: =  $rayfloor/floor_ray2
-export var speed: = 1000
-export var minspeed: = 100
-export var fricction: = 0.1
-export var acceleration: = 10
+
+export var maxspeed: = 400
+export var minspeed: = 50
+export var fricction: = 4
+export var acceleration: = 2
 export var jumpheight: = 1000
 
 onready var jump: float = ((2.0 * jumpheight) / jumptime) * -1.0
@@ -28,13 +29,17 @@ var motion: = 0
 var anim_time: float
 var on_air: bool
 var is_atacking: bool
+var one_time: = false
+var act_speed: = 0
 
 func _ready() -> void:
 	pass 
 
 func _physics_process(delta: float) -> void:
+	
 	direction.y = gravity() * delta
-	direction.x = get_directions() * delta
+	
+	get_directions(delta)
 	
 	animations()
 	
@@ -44,48 +49,50 @@ func _physics_process(delta: float) -> void:
 	direction = move_and_slide(direction, Vector2.UP)
 	
 
-
-func get_directions():
+func get_directions(delta):
 	if !is_atacking or on_air:
-		if Input.is_action_pressed("Left"):
-			direction.x = move_toward(-direction.x, -speed, acceleration)
-			print(direction.x)
+		
+		if Input.is_action_pressed("Left") and direction.x > -maxspeed:
+			direction.x += lerp(direction.x, -maxspeed, acceleration) * delta
 			motion = -1
-		elif Input.is_action_pressed("Right"):
-			direction.x = lerp(direction.x, speed, acceleration)
-			print(direction.x)
+		elif Input.is_action_pressed("Right") and direction.x < maxspeed:
+			direction.x += lerp(direction.x, maxspeed, acceleration) * delta
 			motion = 1
-		else:
-			direction.x = lerp(direction.x, 0, fricction)
-			print(direction.x)
-	else:
-		direction.x = lerp(direction.x, 0, fricction)
-	return direction.x
+		elif direction.x < 0:
+			direction.x += lerp(direction.x, 0, fricction) * delta
+			motion = 0
+		elif direction.x > 0:
+			direction.x += lerp(direction.x, 0, fricction) * delta
+			motion = 0
+	elif direction.x < 0:
+		direction.x += lerp(direction.x, 0, fricction) * delta
+	elif direction.x > 0:
+		direction.x += lerp(direction.x, 0, fricction) * delta
 
 func animations():
 	if (!anim_player.is_playing() or current_anim == "idle") and (Input.is_action_just_pressed("Attack") or Input.is_action_just_pressed("Specials")):
 		atacks()
-	elif !is_atacking and !is_on_floor() and Input.is_action_just_pressed("Jump"):
+	elif !is_atacking and on_air and !one_time:
 		anim_player.play("Jump")
-		anim_time = anim_player.get_current_animation_position()
+		anim_player.advance(0)
+		one_time = true
 		if floor_ray1.is_colliding() or floor_ray2.is_colliding():
 			anim_player.play("idle")
-		elif on_air and anim_time == 0:
-			anim_player.play("falling")
-	elif !is_atacking and motion >= 0 and Input.is_action_pressed("Right"):
+	elif !is_atacking and motion == 1 and !on_air:
 		anim_player.play("walk")
-	elif !is_atacking and motion <= 0 and Input.is_action_pressed("Left"):
+	elif !is_atacking and motion == -1 and !on_air:
 		anim_player.play("walkL")
 	elif !anim_player.is_playing():
 		anim_player.play("idle")
+		one_time = false
 		is_atacking = false
-
 
 func jumping():
 	direction.y += jump
-	on_air = true
-	if is_on_floor():
+	if is_on_floor() or floor_ray1.is_colliding() or floor_ray2.is_colliding():
 		on_air = false
+	else:
+		on_air = true
 
 func gravity():
 	return jumpfall if jumping() else grav
