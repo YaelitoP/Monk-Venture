d35c602b45b5db043d98df437d186e39eeb1d955
Yael <gabypelle12@gmail.com>
2022-08-16 16:43:38 -0300

    dmg manager start and pollish wispy spawn

--- Script/wizar_Mob.gd
+++ Script/wizar_Mob.gd
@@ -1,4 +1,8 @@
-extends KinematicBody2D
+extends RigidBody2D
+
+
+signal health_update(health)
+signal death()
 
 onready var anim: = $anim_wizard
 onready var area: = $area_wizard
@@ -9,15 +13,17 @@ onready var aim1: = $aiming/aim2
 onready var aim2: = $aiming/aim3
 onready var shot_speed: = $shot_speed
 onready var bullet: = preload("res://tscn/wispy.tscn")
-
+onready var parent: = get_parent()
 var bullet_speed: = 500
 var angle_shot: = Vector2.ZERO
 var collision0: Node
 var collision1: Node
 var collision2: Node
 
+var health: = 100
+var max_health: = 200
 var sight: = 100
-
+var dmg_income = 0
 var intruder_pos: Vector2
 
 var intruder: Object
@@ -30,29 +36,33 @@ func _physics_process(delta: float) -> void:
 	
 	sprites()
 	
+	for entry_signal in get_incoming_connections():
+		if entry_signal.signal_name == "monkdmg":
+			dmg_income = entry_signal.signal_name.monkdmg
+
+
 	for collide in area.get_overlapping_bodies():
 		if collide.is_in_group("player"):
-			
 			check_cast_to(collide)
 			if is_shooting == false:
 				is_shooting = true
+				yield(get_tree().create_timer(0.85), "timeout")
 				var bullet_shot: = bullet.instance()
-				add_child(bullet_shot)
+				parent.add_child(bullet_shot)
 				shooting(collide, bullet_shot)
-				yield(get_tree().create_timer(1.0), "timeout")
 				is_shooting = false
 
 
-func shooting(collide, bullet_shot):
-	if collide.is_in_group("player"):
+func shooting(objective, bullet_shot):
+	if objective.is_in_group("player"):
 		bullet_shot.to_local(global_position)
-		bullet_shot.set_rotation_degrees(bullet_shot.get_angle_to(collide.global_position))
+		bullet_shot.set_rotation_degrees(bullet_shot.get_angle_to(objective.global_position))
 		bullet_shot.apply_impulse(Vector2.ZERO, Vector2(bullet_speed, 0).rotated(aim1.get_cast_to().angle()))
 
 func _on_area_wizard_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		intruder = body
-		
+
 func check_cast_to(collide):
 	intruder_pos = global_position.direction_to(collide.get_position()) * sight
 	
@@ -71,3 +81,15 @@ func sprites():
 		anim.play("idle")
 	elif is_shooting:
 		anim.play("shoot")
+
+
+
+func _on_wizard_monk_dmg(dmg) -> void:
+	dmg_income = dmg
+
+
+func _on_hurbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("atacks"):
+		health = health - dmg_income
+		emit_signal("health_update", health)
+		print(health)
